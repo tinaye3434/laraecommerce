6eb61934b792513c073825c4850b1810f02291d3
Tinaye Katemba <64467034+tinaye3434@users.noreply.github.com>
2023-09-24 12:50:30 +0200

    Logout Implemented

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -47,20 +47,20 @@
     
 
 
-    <script src="{{ asset('admin/vendors/base/vendor.bundle.base.js') }}"></script>
+    <script src="{{ asset('admins/vendors/base/vendor.bundle.base.js') }}"></script>
 
-    <script src="{{ asset('admin/vendors/datatables.net/jquery.dataTables.js') }}"></script>
-    <script src="{{ asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
+    <script src="{{ asset('admins/vendors/datatables.net/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('admins/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
 
-    <script src="{{ asset('admin/js/off-canvas.js') }}"></script>
-    <script src="{{ asset('admin/js/hoverable-collapse.js') }}"></script>
-    <script src="{{ asset('admin/js/template.js') }}"></script>
+    <script src="{{ asset('admins/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('admins/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('admins/js/template.js') }}"></script>
 
     <!-- Custom js for this page-->
-    <script src="{{ asset('admin/js/dashboard.js') }}"></script>
-    <script src="{{ asset('admin/js/data-table.js') }}"></script>
-    <script src="{{ asset('admin/js/jquery.dataTables.js') }}"></script>
-    <script src="{{ asset('admin/js/dataTables.bootstrap4.js') }}"></script>
+    <script src="{{ asset('admins/js/dashboard.js') }}"></script>
+    <script src="{{ asset('admins/js/data-table.js') }}"></script>
+    <script src="{{ asset('admins/js/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('admins/js/dataTables.bootstrap4.js') }}"></script>
     <!-- End custom js for this page-->
     @livewireScripts
 </body>
